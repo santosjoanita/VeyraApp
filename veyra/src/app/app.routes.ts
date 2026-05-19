29c7315560d0b0d0e63d9f5ca56d1b8b46d961b4
santosjoanita <santos.joana@ipvc.pt>
2026-05-19 14:07:59 +0100

    criação de pastas de acordo com a estrutura +  rotas iniciais públicas e privadas

--- veyra/src/app/app.routes.ts
+++ veyra/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redireciona logo para o login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  //página de login, ou pública
+  { 
+    path: 'login', 
+    loadComponent: () => import('./pages/public/login/login').then(m => m.Login)
+  },
+
+  //private
+  {
+    path: 'app',
+    loadComponent: () => import('./pages/private/main-layout/main-layout').then(m => m.MainLayout),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/private/dashboard/dashboard').then(m => m.Dashboard)
+      }
+    ]
+  }
+];
