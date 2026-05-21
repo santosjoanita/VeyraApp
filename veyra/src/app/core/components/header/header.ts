137a6a7a8a28b17d22a37355aa22ea4a68b61ae3
santosjoanita <santos.joana@ipvc.pt>
2026-05-21 18:12:36 +0100

    header pronto para receber a api

--- veyra/src/app/core/components/header/header.ts
+++ veyra/src/app/core/components/header/header.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  currentUser = {
+    name: 'Dexter Morgan',
+    role: 'admin'
+  };
+
+  logout() {
+    console.log('User signed out');
+
+  }
+}
