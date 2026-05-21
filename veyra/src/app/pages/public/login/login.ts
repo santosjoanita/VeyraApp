c20e1bf12e751d6f08aa650720bf1b3afdbc1ff1
santosjoanita <santos.joana@ipvc.pt>
2026-05-21 08:29:49 +0100

    mudanças estéticas

--- veyra/src/app/pages/public/login/login.ts
+++ veyra/src/app/pages/public/login/login.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
