ef7c8d00b3d2baefd5a90e8d0452f6019942610e
santosjoanita <santos.joana@ipvc.pt>
2026-05-26 09:29:35 +0100

    Criação da página clients

---
+++ veyra/src/app/pages/private/clients/clients.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  imports: [],
+  templateUrl: './clients.html',
+  styleUrl: './clients.css',
+})
+export class Clients {}
