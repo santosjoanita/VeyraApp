23278750af514d3c87a7faee5296a506d72b96d1
santosjoanita <santos.joana@ipvc.pt>
2026-05-28 08:34:22 +0100

    Estruturas base de cada um dos modais

--- veyra/src/app/core/components/modals/add-project/add-project.ts
+++ veyra/src/app/core/components/modals/add-project/add-project.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-project',
-  imports: [],
+  selector: 'app-add-project-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-project.html',
-  styleUrl: './add-project.css',
+  styleUrls: []
 })
-export class AddProject {}
+export class AddProjectModal {
+  @Input() clients: any[] = []; 
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<any>();
+
+  formData = {
+    clientId: '',
+    name: '',
+    description: '',
+    status: 'active',
+    startDate: '',
+    endDate: ''
+  };
+
+  submitForm() {
+    this.save.emit(this.formData);
+  }
+
+  cancel() {
+    this.close.emit();
+  }
+}
--- veyra/src/app/core/components/modals/add-worker/add-worker.ts
+++ veyra/src/app/core/components/modals/add-worker/add-worker.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-worker',
-  imports: [],
+  selector: 'app-add-worker-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-worker.html',
-  styleUrl: './add-worker.css',
+  styleUrls: []
 })
-export class AddWorker {}
+export class AddWorkerModal {
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<any>();
+
+  formData = {
+    name: '',
+    email: '',
+    password: '',
+    role: 'worker' //está este por default mas depois o admin pode mudar 
+  };
+
+  submitForm() {
+    this.save.emit(this.formData);
+  }
+
+  cancel() {
+    this.close.emit();
+  }
+}
