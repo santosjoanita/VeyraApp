2aaeaa42a34a98c17cbea27d39f385b6834de851
santosjoanita <santos.joana@ipvc.pt>
2026-05-28 10:24:44 +0100

    Modal de delete

--- veyra/src/app/core/components/modals/confirm-delete/confirm-delete.ts
+++ veyra/src/app/core/components/modals/confirm-delete/confirm-delete.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-delete',
+  standalone: true,
   imports: [],
   templateUrl: './confirm-delete.html',
   styleUrl: './confirm-delete.css',
 })
-export class ConfirmDelete {}
+export class ConfirmDelete {
+  @Input() itemName: string = 'this item'; 
+  
+  @Output() confirm = new EventEmitter<void>();
+  
+  @Output() close = new EventEmitter<void>();
+
+  confirmDelete() {
+    this.confirm.emit();
+  }
+
+  cancel() {
+    this.close.emit();
+  }
+}
